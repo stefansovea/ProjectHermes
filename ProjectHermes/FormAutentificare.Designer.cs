1ba72a0553d758da1fda86d1ad05a4ca39f52bb4
LAPTOP-TTHRCJDC\enach <enachiuc.marcela@yahoo.com>
2021-11-21 17:06:06 +0200

    Autentificare+Profesor+Elev+Secretar

---
+++ ProjectHermes/FormAutentificare.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ProjectHermes
+{
+    partial class FormAutentificare
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAutentificare));
+            this.logInButton = new System.Windows.Forms.Button();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.adviceLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // logInButton
+            // 
+            this.logInButton.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.logInButton.Location = new System.Drawing.Point(85, 290);
+            this.logInButton.Name = "logInButton";
+            this.logInButton.Size = new System.Drawing.Size(229, 74);
+            this.logInButton.TabIndex = 0;
+            this.logInButton.Text = "Log In";
+            this.logInButton.UseVisualStyleBackColor = true;
+            this.logInButton.Click += new System.EventHandler(this.logInButton_Click);
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.nameLabel.Location = new System.Drawing.Point(80, 159);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(138, 27);
+            this.nameLabel.TabIndex = 2;
+            this.nameLabel.Text = "Nume Utilizator";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.passwordLabel.Location = new System.Drawing.Point(80, 214);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(65, 27);
+            this.passwordLabel.TabIndex = 3;
+            this.passwordLabel.Text = "Parola";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(85, 189);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(229, 22);
+            this.nameTextBox.TabIndex = 4;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.passwordTextBox.Location = new System.Drawing.Point(85, 244);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(229, 22);
+            this.passwordTextBox.TabIndex = 5;
+            this.passwordTextBox.UseSystemPasswordChar = true;
+            // 
+            // adviceLabel
+            // 
+            this.adviceLabel.AutoSize = true;
+            this.adviceLabel.Font = new System.Drawing.Font("Arial Narrow", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.adviceLabel.Location = new System.Drawing.Point(82, 269);
+            this.adviceLabel.Name = "adviceLabel";
+            this.adviceLabel.Size = new System.Drawing.Size(29, 16);
+            this.adviceLabel.TabIndex = 6;
+            this.adviceLabel.Text = "NaN";
+            this.adviceLabel.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(135, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(120, 120);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormAutentificare
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 413);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.adviceLabel);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.logInButton);
+            this.MaximizeBox = false;
+            this.Name = "FormAutentificare";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button logInButton;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Label adviceLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ProjectHermes/FormAutentificare.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectHermes
+{
+    public partial class FormAutentificare : Form
+    {
+        //Hardcodare user
+        string profesorUsername = "profesor"; string profesorPassword = "profesor";
+        string elevUsername = "elev"; string elevPassword = "elev";
+        string secretarUsername = "secretar"; string secretarPassword = "secretar";
+        //Hardcodare user
+
+        public FormAutentificare()
+        {
+            InitializeComponent();
+        }
+
+        private void ValidareImput(String nume, String parola)
+        {
+            const int nrCaractere = 4;
+
+            if (nume.Length < nrCaractere)
+            {
+                adviceLabel.Visible = true;
+                adviceLabel.ForeColor = Color.Red;
+                adviceLabel.Text = nameLabel.Text + " trebuie sa contina cel putin " + nrCaractere + " caractere";
+            }
+            else if (parola.Length < nrCaractere)
+            {
+                adviceLabel.Visible = true;
+                adviceLabel.ForeColor = Color.Red;
+                adviceLabel.Text = passwordLabel.Text + " trebuie sa contina cel putin " + nrCaractere + " caractere";
+            }
+            else if (nume == secretarUsername || nume == elevUsername || nume == profesorUsername)
+            {
+                adviceLabel.Visible = true;
+                adviceLabel.ForeColor = Color.Red;
+                adviceLabel.Text = "Parola incorecta!";
+            }
+            else if (nume != secretarUsername || nume != elevUsername || nume != profesorUsername)
+            {
+                adviceLabel.Visible = true;
+                adviceLabel.ForeColor = Color.Red;
+                adviceLabel.Text = "Numele de utilizator nu este inregistrat!";
+            }
+            else
+            {
+                adviceLabel.Visible = true;
+                adviceLabel.ForeColor = Color.Red;
+                adviceLabel.Text = "Exceptie nerezolvata!";
+            }
+        }
+
+        private void logInButton_Click(object sender, EventArgs e)
+        {
+            //Redirectionare catre formularul corespunzator
+            if (nameTextBox.Text == profesorUsername && passwordTextBox.Text == profesorPassword)
+            {
+                FormProfesor formProfesor = new FormProfesor();
+                formProfesor.Show();
+                this.Hide();
+            }
+            else if(nameTextBox.Text == elevUsername && passwordTextBox.Text == elevPassword)
+            {
+                FormElev formElev = new FormElev();
+                formElev.Show();
+                this.Hide();
+            }
+            else if (nameTextBox.Text == secretarUsername && passwordTextBox.Text == secretarPassword)
+            {
+                FormSecretar formSecretar = new FormSecretar();
+                formSecretar.Show();
+                this.Hide();
+            }
+            else
+            {
+                ValidareImput(nameTextBox.Text, passwordTextBox.Text);
+            }
+        }
+    }
+}
