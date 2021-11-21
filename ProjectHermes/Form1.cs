6e024307f2f1bb8e3833110fad42f91dcb0a8364
stefansovea <62215821+stefansovea@users.noreply.github.com>
2021-11-21 02:07:03 +0200

    database test

--- ProjectHermes/Form1.cs
+++ ProjectHermes/Form1.cs
@@ -1,10 +1,33 @@
+using Microsoft.Data.SqlClient;
+using System;
+
+
 namespace ProjectHermes
 {
     public partial class Form1 : Form
     {
+        SqlConnection con = new SqlConnection("Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename=C:\\Users\\Stefan\\Documents\\GitHub\\ProjectHermes\\ProjectHermes\\Database1.mdf;Integrated Security = True");
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from elevi");
+            cmd.Connection = con;
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                MessageBox.Show("Connected");
+                con.Close();
+            }
+            else
+            {
+                MessageBox.Show("Error");
+                con.Close();
+            }
         }
     }
 }
